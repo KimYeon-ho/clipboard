a3c71588c6832f272a5736b6fbca0fcecec515c6
jaden <jaden1.kim@cheilpengtai.com>
2018-06-19 15:44:34 +0900

    add

---
+++ README.md
@@ -0,0 +1,48 @@
+# clipboard.js
+- clipboardData, execCommand 메서드를 통한 클립보드 복사 기능 구현.
+- 미지원 브라우저에서 prompt 창을 통한 카피텍스트 노출.
+
+## 사용법
+
+>기본
+
+```javascript
+new Clipboard([[selector]], {
+    text: [[message]],
+    promptMsg: [[prompt information message]],
+    callback: [[callback function]]
+});
+```
+>Options
+
+```
+@text // 복사할 키워드
+@promptMsg // prompt 창 안내 메시지 (default: '해당 문구를 복사해주세요.')
+@callback // 콜백 함수 (default: function(){ alert('클립보드에 해당 문구가 복사되었습니다.')} )
+```
+>Method
+
+```
+Clipboard.copy() // 메서드를 통한 복사 실행
+Clipboard.initOption(options) // 옵션 변경
+Clipboard.reset() // 클립보드 기능 제거(초기화)
+```
+
+
+## 주의사항
+- 선택자 인식 우선순위 : querySelector > $(selector) > getElementById
+- **jQuery 라이브러리가 없는 IE8 이하 환경**에서는 **id 셀렉터만 허용**된다.
+
+## 브라우저 호환
+### Desktop
+| |  <center>IE</center> |  <center>Chrome</center> |  <center>Firefox</center> | <center>Safari</center> |
+------------ | ------------- | ------------- | ------------- | -------------
+|**ClipboardData** |<center>Support</center> | <center>X</center> |<center>X</center> |<center>X</center> |
+|**execCommand** | <center>9+</center> | <center>42+</center> | <center>41+</center> | <center>10+</center> |
+
+### Mobile
+| |  <center>Samsung Internet</center> |  <center>Chrome for Android</center> |  <center>Firefox Mobile</center> | <center>Safari Mobile</center> |
+ ------------- | ------------- | ------------- | ------------- | -------------
+|**ClipboardData** | <center>X</center> | <center>X</center> |<center>X</center> |<center>X</center> |
+|**execCommand** | <center>6.4+</center> | <center>42+</center> | <center>41+</center> | <center>10+</center> |
+*SamsungInternet : 6.4 미만 버전 체크 필요*
---
+++ js/clipboard.js
@@ -0,0 +1,155 @@
+/**
+ * @param selector
+ * @param options
+ * @constructor
+ */
+function Clipboard(selector, options) {
+    this.button = typeof document.querySelector === 'function' ? document.querySelector(selector) : window.$ ? $(selector)[0] : document.getElementById(selector.split('').slice(1).join(''));
+    this._options = null;
+    this._txtBox = null;
+    this._success = true;
+    this._isPrompt = false;
+    this._copyEvent = null;
+    this.initOption(options);
+    this._initEvent();
+}
+/**
+ * @type {{
+ * text: string,
+ * promptMsg: string,
+ * callback: Clipboard._defaultOptions.callback
+ * }}
+ * @private
+ */
+Clipboard._defaultOptions = {
+    text: '복사할 키워드를 입력해주세요.', // 복사 대상 키워드
+    promptMsg: '해당 문구를 복사해주세요.', // 프롬포터 창 메시지
+    callback: function () {
+        alert('클립보드에 해당 문구가 복사되었습니다.');
+    } // 콜백 기능
+};
+/**
+ * @type {{
+ * reset: Clipboard.reset,
+ * _removeEvent: Clipboard._removeEvent,
+ * initOption: Clipboard.initOption,
+ * _initEvent: Clipboard._initEvent,
+ * copy: Clipboard.copy,
+ * _setClipboardData: Clipboard._setClipboardData,
+ * _createTxtBox: Clipboard._createTxtBox,
+ * _setExecCommand: Clipboard._setExecCommand,
+ * _complete: Clipboard._complete
+ * }}
+ */
+Clipboard.prototype = {
+    // 이벤트 제거
+    reset: function () {
+        if ( !this.button ) return false;
+        this._removeEvent();
+        this.button = null;
+        this._options = null;
+        this._txtBox = null;
+        this._success = null;
+        this._isPrompt = null;
+        this._copyEvent = null;
+    },
+    _removeEvent: function () {
+        var that = this;
+        if (window.removeEventListener) {
+            this.button.removeEventListener('click', that._copyEvent);
+        } else {
+            this.button.detachEvent('onclick', that._copyEvent);
+        }
+    },
+    // 옵션 설정
+    initOption: function (options) {
+        options = options || {text: undefined, promptMsg: undefined, callback: undefined};
+        this._options = {
+            text: options.text !== undefined ? options.text : Clipboard._defaultOptions.text,
+            promptMsg: options.promptMsg !== undefined ? options.promptMsg : Clipboard._defaultOptions.promptMsg,
+            callback: options.callback !== undefined ? options.callback : Clipboard._defaultOptions.callback
+        };
+    },
+    _initEvent: function () {
+        var that = this;
+        this._copyEvent = this.copy.bind(this);
+        if (window.addEventListener) {
+            this.button.addEventListener('click', that._copyEvent);
+        } else {
+            this.button.attachEvent('onclick', that._copyEvent);
+        }
+    },
+    // 클립보드 복사 실행
+    copy: function () {
+        if ( !this.button ) return false;
+        if (window.clipboardData) {
+            this._setClipboardData();
+        } else {
+            this._createTxtBox();
+            this._setExecCommand();
+        }
+        this._complete();
+    },
+    _setClipboardData: function () {
+        window.clipboardData.setData('Text', this._options.text);
+    },
+    _createTxtBox: function () {
+        var txtBox = document.createElement('textarea');
+        txtBox.style.cssText = 'overflow:hidden;position:fixed;top:-1px;left:-1px;width:1px;height:1px;';
+        this._txtBox = this.button.parentNode.appendChild(txtBox);
+        this._txtBox.innerHTML = this._options.text;
+        this._txtBox.select();
+    },
+    _setExecCommand: function () {
+        try {
+            this._success = document.execCommand('copy', false, null);
+        }
+        catch (e) {
+            this._isPrompt = true;
+            if (!window.prompt(this._options.promptMsg, this._options.text)) {
+                this._success = false;
+            }
+        }
+        finally {
+            this._txtBox.remove();
+        }
+    },
+    _complete: function () {
+        if (this._success) {
+            this._options.callback();
+        } else {
+            if (!this._isPrompt) {
+                if (window.prompt(this._options.promptMsg, this._options.text)) {
+                    this._options.callback();
+                }
+            }
+        }
+    }
+};
+/**
+ * Polyfill
+ */
+if (!Function.prototype.bind) {
+    Function.prototype.bind = function(oThis) {
+        if (typeof this !== 'function') {
+            // ECMAScript 5 내부 IsCallable 함수와
+            // 가능한 가장 가까운 것
+            throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+        }
+        var aArgs   = Array.prototype.slice.call(arguments, 1),
+            fToBind = this,
+            fNOP    = function() {},
+            fBound  = function() {
+                return fToBind.apply(this instanceof fNOP
+                    ? this
+                    : oThis,
+                    aArgs.concat(Array.prototype.slice.call(arguments)));
+            };
+        if (this.prototype) {
+            // Function.prototype은 prototype 속성이 없음
+            fNOP.prototype = this.prototype;
+        }
+        fBound.prototype = new fNOP();
+        return fBound;
+    };
+}
